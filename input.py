be3eef8f655921d73da0ba9d2eeb4682fac1f0fc
Jakob Schonberg <quorthon44@gmail.com>
2026-08-28 09:28:58 +0200

    Input handling for various data types

---
+++ input.py
@@ -0,0 +1,8 @@
+s = input("Enter a string: ")
+x = int(input("Enter an int: "))
+l = input("Enter one or more space sperated strings: ").split()
+n, m = map(int, input("Enter 2 ints: ").split())
+tmp = input("Enter a string and an int: ").split()
+o = tmp[0]
+p = int(tmp[1])
+x = complex(input("Enter a complex number: "))
