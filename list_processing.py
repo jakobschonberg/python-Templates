453328ad8692dae3129846f829fa34d3401337b8
Jakob Schonberg <quorthon44@gmail.com>
2026-09-16 09:17:57 +0200

    more list comprehension

--- list_processing.py
+++ list_processing.py
@@ -3,6 +3,7 @@ v = map(int, s)
 p = all(i > 0 for i in v) and any(j == j[::-1] for j in s)
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+doubled_numbers = [2 * numbers for number in numbers]
 evens = filter(lambda x: x % 2 == 0, numbers)
 
 fruits = ['apple', 'banana', 'cherry']
