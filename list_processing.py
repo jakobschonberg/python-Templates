28672e2147579c3284aa7f10358fc05f4204183b
Jakob Schonberg <quorthon44@gmail.com>
2026-09-16 09:26:32 +0200

    alternative to filter lambda

--- list_processing.py
+++ list_processing.py
@@ -5,6 +5,7 @@ p = all(i > 0 for i in v) and any(j == j[::-1] for j in s)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 doubled_numbers = [2 * numbers for number in numbers]
 evens = filter(lambda x: x % 2 == 0, numbers)
+evens = [number for number in numbers if number % 2 == 0]
 
 fruits = ['apple', 'banana', 'cherry']
 lengths = list(map(lambda x: len(x), fruits))
